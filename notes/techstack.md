724accefe776fe458d93bc40a9fa7725916f5c6b
jojewar Dattatray <jojewardattatray@gmail.com>
2026-04-05 01:12:43 +0530

    added watcher function, and updated csv parserer function

---
+++ notes/techstack.md
@@ -0,0 +1,6 @@
+# Tech Stack
+
+### Python
+### watchdog
+### navisworks
+### 
--- src/parser_csv.py
+++ src/parser_csv.py
@@ -1,11 +1,27 @@
 import csv
+import json
 
 
-def parcer_csv():
-    with open("D:/My_Learning/Non-Coders/data/meshdata.csv", mode="r") as file:
+def parse_csv():
+
+    csv_path = "D:/My_Learning/Non-Coders/data/meshdata.csv"
+    json_path = "D:/My_Learning/Non-Coders/data/data.json"
+
+    data = []
+
+    with open(csv_path, mode="r") as file:
         reader = csv.DictReader(file)
 
         for row in reader:
-            print(row)
+            data.append(row)
 
-    return reader
+    with open(json_path, "w") as jsonfile:
+        json.dump(data, jsonfile, indent=4)
+
+    return data
+
+
+# Run function
+if __name__ == "__main__":
+    result = parse_csv()
+    print("Total rows:", len(result))
